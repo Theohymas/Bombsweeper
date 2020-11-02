90a1d1930e5de1e576c83f9c71aa0e4eb1591a05
Theo Hymas <theohymas@aol.com>
2020-11-02 17:51:11 +0000

    first commit

---
+++ BombSquare.java
@@ -0,0 +1,35 @@
+public class BombSquare extends GameSquare
+{
+    private GameBoard board;                            // Object reference to the GameBoard this square is part of.
+    private boolean hasBomb;                            // True if this squre contains a bomb. False otherwise.
+
+	public static final int MINE_PROBABILITY = 10;
+
+	public BombSquare(int x, int y, GameBoard board)
+	{
+		super(x, y, "images/blank.png");
+
+        this.board = board;
+        this.hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
+    }
+
+    public void leftClicked()
+    {
+        if(this.hasBomb == true)
+        {
+            System.out.println("bomb clicked");
+            this.setImage("images/bomb.png");
+        }
+    }
+
+    public void rightClicked()
+    {
+        hasBomb = true;
+        if(this.hasBomb == true)
+        {
+            System.out.println("bomb clicked");
+            this.setImage("images/bomb.png");
+        }
+    }
+}
+
